8a7e8fdfa47628af86f834d4bf5eb3d0dae928c5
KaziShadmanSakib <shadmansakib.24csedu.010@gmail.com>
2020-10-22 11:05:21 +0600

    1st Day

---
+++ towerOfHanoi.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int towerOfHanoi(int n){
+	int y;
+	if(n==0){
+		return 1;
+	}
+	else{
+		y = 2*towerOfHanoi(n-1);
+		return y+1;
+	}
+}
+int main()
+{
+	int n,res;
+	cin>>n;
+	res = towerOfHanoi(n);
+	cout<<res<<endl;
+	return 0;
+}
