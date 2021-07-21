251c34bb3972728b29ae855a5960fc155a3701bf
KaziShadmanSakib <shadmansakib.24csedu.010@gmail.com>
2021-07-21 23:35:00 +0600

    Rabbit Recursion

---
+++ rabitFibonacci.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fib(int n){
+
+	if(n<3){
+
+		return 1;
+
+	}
+
+	return fib(n-1) + fib(n-2);
+
+}
+
+int main()
+{
+
+	int n;
+
+	cin>> n;
+
+	int ans = fib(n);
+
+	cout<< ans << endl;
+
+	return 0;
+
+}
